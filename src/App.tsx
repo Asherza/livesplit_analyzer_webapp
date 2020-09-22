2dd4e0719546d4f5f3f35db2a34b721e0c28eab3
odevine <hello@ode.moe>
2020-09-22 16:19:15 -0400

    Add warning comment for body text

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { DrawerNavigation } from "@components";
 
 const App = () => <div>
   <DrawerNavigation />
-  test
+  {/* The text below will eventually be wrapped in a body component, until then it the nav drawer will overlap */}
+  Asher is really good at speedrunning portal
 </div>;
 
 export default hot(App);
